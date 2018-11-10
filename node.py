e056f60a782418696804ef34eaefa43f3dd96cb8
edkb <eduk_binoto@hotmail.com>
2018-11-10 19:45:52 -0200

    Leader election

--- node.py
+++ node.py
@@ -3,8 +3,7 @@ import time
 import socket
 import signal
 import sys
-
-from contextlib import contextmanager
+import json
 
 
 class ServerNode:
@@ -12,19 +11,19 @@ class ServerNode:
     state = ['Follower', 'Candidate', 'Leader']
 
     nodos = {
-        'node1': {'id': 'a', 'port': 5001},
-        'node1': {'id': 'b', 'port': 5002},
-        'node1': {'id': 'c', 'port': 5003},
-        'node1': {'id': 'd', 'port': 5004},
-        'node1': {'id': 'e', 'port': 5005},
+        'node1': {'name': 'a', 'port': 5001},
+        'node2': {'name': 'b', 'port': 5002},
+        'node3': {'name': 'c', 'port': 5003},
+        'node4': {'name': 'd', 'port': 5004},
+        'node5': {'name': 'e', 'port': 5005},
     }
 
     def __init__(self, node_id, node_port):
 
-        self._name = node_id     # Identification of the server node
-        self._state = 'Follower' # Every server starts as follower
+        self._name = node_id        # Identification of the server node
+        self._state = 'Follower'    # Every server starts as follower
 
-        self.PORT = node_port    # Arbitrary port for the server
+        self.PORT = int(node_port)   # Arbitrary port for the server
 
         # The election timeout is the amount of time a follower waits until becoming a candidate.
         self._election_timeout = self.set_election_timeout()  # Sets node election timeout
@@ -53,58 +52,43 @@ class ServerNode:
         print(f'Starting node {self._name} listening on port {self.PORT} as {self._state}')
 
         self.config_timeout()
-        self.idle()
+        self.receive_msg()
 
     def idle(self):
+        self.receive_msg()
+        '''
+        if timeout_type == 'Election':
+            print('Starting a new election')
+            # After the election timeout the follower becomes a candidate and starts a new election term...
+            self.start_election()
 
-        try:
-            while True:
-                pass
-        except TimeoutError as e:
-            print(e)
-
-            if e.args == 'Election':
-                # After the election timeout the follower becomes a candidate and starts a new election term...
-                self.be_candidate()
-
-            elif e.args == 'Hearbeat':  # These messages are sent in intervals specified by the heartbeat timeout.
-                #  ...then the change is sent to the followers on the next heartbeat.
-                self.append_entries()      # The leader begins sending out Append Entries messages to its followers.
-
-            self.idle()
+        elif timeout_type == 'Hearbeat':  # These messages are sent in intervals specified by the heartbeat timeout.
+            #  ...then the change is sent to the followers on the next heartbeat.
+            self.append_entries()      # The leader begins sending out Append Entries messages to its followers.
+        '''
 
     def config_timeout(self):
-        print("timeout config")
 
-        if self._state == 'Follower':
-            print(f'Configured follower node {self._name} with election timout to: {self._election_timeout}')
+        if self._state in ['Follower', 'Candidate']:
+            print(f'Configured follower node {self._name} with election timeout to: {self._election_timeout}')
             signal.signal(signal.SIGALRM, self.election_timeout_handler)
             signal.alarm(self._election_timeout)
 
-        elif self.state == 'Candidate':
-            print(f'Configured candidate node {self._name} with heartbeat timout to: {self.heartbeat_timeout}')
+        elif self._state == 'Leader':
+            print(f'Configured candidate node {self._name} with heartbeat timeout to: {self._heartbeat_timeout}')
             signal.signal(signal.SIGALRM, self.heartbeat_timeout_handler)
             signal.alarm(self._heartbeat_timeout)
 
-    def be_candidate(self):
-        self._state = 'Candidate'
-        # for nodes in cluter:
-        self.request_vote()
-
-        # Starts new election term
-        # Votes for itself
-        # Sends request vote messages for other nodes
-
     def election_timeout_handler(self, signum, frame):
         print("Reached election timeout!")
-        raise TimeoutError('Election')
+        self.start_election()
 
     def heartbeat_timeout_handler(self, signum, frame):
         print("Reached hearbeat timeout!")
         raise TimeoutError('Heartbeat')
 
     # Remote procedure call
-    def request_vote(self):
+    def request_vote(self, node_port):
 
         msg = {
             'term': self._current_term,
@@ -113,10 +97,14 @@ class ServerNode:
             'last_log_term': self._commit_index
         }
 
-        reply = self.send_msg(msg)
+        reply = self.send_msg(msg, node_port)
 
         # Imprime os dados recebidos
         print('Recebido: ', repr(reply))
+
+        if reply['canidate_id'] == self.__name:
+            self._votes_in_term += 1
+            print('Votes in term')
 
         # Once a candidate has a majority of votes it becomes leader.
         if self._votes_in_term > 2:
@@ -128,11 +116,30 @@ class ServerNode:
         :param msg:
         :return:
         """
-        if self._voted_for is not None:
-            if msg['term'] == self._current_term:
-                reply_vote = {'candidate_id': msg['candidate_id']}
-                self.send_msg(reply_vote)
-                self.set_election_timeout()  # ...and the node resets its election timeout.
+        if msg['term'] > self._current_term:
+
+            self._state = "Follower"  # Becomes follower again if term is outdated
+
+            self._current_term = msg['term']
+            self._voted_for = msg['candidate_id']
+            reply_vote = {
+                'candidate_id': msg['candidate_id']
+            }
+            self.set_election_timeout()  # ...and the node resets its election timeout.
+            return json.dumps(reply_vote)
+
+        else:
+            if self._voted_for is None:
+                reply_vote = {
+                    'candidate_id': msg['candidate_id']
+                }
+            else:
+                reply_vote = {
+                    'candidate_id': self._voted_for
+                }
+            return json.dumps(reply_vote)
+
+
 
     def vote_in_candidate(self, candidate):
         pass
@@ -147,9 +154,9 @@ class ServerNode:
         """
         Set a new election timeout for follower node
 
-        :return: timeout between 5 and 10 seconds
+        :return: timeout between 3 and 5 seconds
         """
-        return round(random.uniform(5, 10))
+        return round(random.uniform(3, 5))
 
     # Remote procedure call
     def append_entries(self):
@@ -177,52 +184,126 @@ class ServerNode:
         pass
 
     def start_election(self):
+        """
+        Starts a new election term
+        :return:
+        """
+        self._state = 'Candidate'
         self._current_term += 1
-        self.request_vote()
+        self.config_timeout()
 
-    def send_msg(self, msg, port, host='localhost'):
+        print(f'Node {self._name} becomes candidate')
+        self._voted_for = self._name
+        self._votes_in_term = 1
 
+        try:
+            # for all nodes in cluster
+            for node, value in self.nodos.items():
+                time.sleep(0.1)
+                if value['name'] != self._name:
+                    try:
+
+                        msg = {
+                            'type': 'req_vote',
+                            'term': self._current_term,
+                            'candidate_id': self._name,
+                            'last_log_index': self._last_applied,
+                            'last_log_term': self._commit_index
+                        }
+                        msg = json.dumps(msg)
+
+                        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
+
+                            # Connects to server destination
+                            tcp.connect(('', value['port']))
+
+                            print(f'Requesting vote to node {node}: {value["name"]}')
+
+                            # Envia mensagem
+                            tcp.sendall(msg.encode('utf-8'))
+
+                            # Recebe dados do servidor
+                            reply = tcp.recv(1024).decode('utf-8')
+
+                            if not reply:
+                                print("Reply not recieved")
+                                break
+
+                            reply = json.loads(reply)
+                            for key, value in reply.items():
+
+                                if value == self._name:
+                                    self._votes_in_term += 1
+                                    print('Votes in term', self._votes_in_term )
+
+                                # Once a candidate has a majority of votes it becomes leader.
+                                if self._votes_in_term > 2:
+                                    self._state = 'Leader'
+                                    print(f'Node {self._name} becomes {self._state}')
+                                    self.receive_msg()
+
+
+                    except Exception as e:
+                        print(e)
+
+                    except KeyboardInterrupt:
+                        raise SystemExit()
+
+        except:
+            return
+
+    def send_msg(self, msg, node_port, host='localhost'):
+
+        print(f'Sending: {msg} to node in port {node_port}')
         # Creates tcp socket
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
 
             # Connects to server destination
-            tcp.connect(host, port)
+            tcp.connect((host, node_port))
 
             # Envia mensagem
-            tcp.sendall(bytearray(msg, 'utf-8'))
+            tcp.sendall(msg.encode('utf-8'))
 
             # Recebe dados do servidor
             data = tcp.recv(1024)
 
-            return data
+        return data
 
     def receive_msg(self):
 
-        # Cria um socket tcp
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
+        while True:
 
-            # Une o socket ao host e a porta
-            tcp.bind(('', self.PORT))  # Recebe mensagens de qualquer host
+            # Cria um socket tcp
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
 
-            # Habilita o servidor a aceitar uma conexao
-            tcp.listen(1)
+                # Une o socket ao host e a porta
+                tcp.bind(('', self.PORT))  # Recebe mensagens de qualquer host
 
-            # Aceita uma conexao. Retorna o objeto do socket (conn) e endereco do cliente (address)
-            conn, address = tcp.accept()
+                # Habilita o servidor a aceitar 5 conexaoes
+                tcp.listen(5)
 
-            with conn:
-                print('Connected by', address)
+                # Aceita uma conexao. Retorna o objeto do socket (conn) e endereco do cliente (address)
+                conn, address = tcp.accept()
 
-                while True:
+                print('socket criado')
 
-                    # Refreshes timeout
-                    with self.config_timeout():
+                with conn:
+                    print('Connected by', address)
+
+                    while True:
 
                         # Recebe os dados do cliente
-                        msg = conn.recv(1024)
+                        msg = conn.recv(1024).decode('utf-8')
 
                         # Se der algo errado com os dados, sai do loop
-                        if not msg: break
+                        if not msg:
+                            print('Something went wrong')
+                            break
+
+                        msg = json.loads(msg)
+
+                        # Imprime os dados recebidos
+                        print('Msg recieved: ', msg)
 
                         # Envia para o cliente os dados recebidos
                         # conn.sendall(data)
@@ -257,13 +338,13 @@ class ServerNode:
                                 'type': 'heart_beat_reply',
                                 'client_if': self._name
                                 }
-                            conn.sendall(reply)
+                            conn.sendall(reply.encode('utf-8'))
 
                         elif msg['type'] == 'req_vote':
-                            self.reply_vote(msg)
+                            reply_msg = self.reply_vote(msg)
+                            print(f'Replying to {msg["candidate_id"]}')
+                            conn.sendall(reply_msg.encode('utf-8'))
 
-                        # Imprime os dados recebidos
-                        print(msg)
 
     def reply_append_entry(self, append_entry_msg):
         """
@@ -278,7 +359,6 @@ class ServerNode:
             'type': 'ack_append_entry'
         }
         self.send_msg(ack_msg, append_entry_msg['port'])
-        pass
 
 
 if __name__ == "__main__":
